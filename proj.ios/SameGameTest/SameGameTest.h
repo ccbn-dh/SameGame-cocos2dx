b06617ab563eb12c462f588de54f9673ae5b7713
nirasan <chibag+nirasan@gmail.com>
2013-09-16 17:46:01 +0900

    add test target

---
+++ proj.ios/SameGameTest/SameGameTest.h
@@ -0,0 +1,13 @@
+//
+//  SameGameTest.h
+//  SameGameTest
+//
+//  Created by 千葉 大二郎 on 2013/09/16.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SameGameTest : SenTestCase
+
+@end
