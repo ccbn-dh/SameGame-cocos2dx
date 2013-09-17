2f603cb51e2b902c7bb8c1bb792e02fcef595348
nirasan <chibag+nirasan@gmail.com>
2013-09-18 06:36:42 +0900

    rename

--- proj.ios/SameGameTest/GameTest.mm
+++ proj.ios/SameGameTest/GameTest.mm
@@ -11,7 +11,7 @@
     STAssertTrue(g->getField()->getBlocks()[FIELD_HEIGHT-1][FIELD_WIDTH-1] == NULL, @"before fill field");
     STAssertTrue(g->getFilledBlocks()[FIELD_HEIGHT-1][FIELD_WIDTH-1] == false, @"before fill field");
     
-    g->fillField();
+    g->fillBlocks();
     
     STAssertTrue(g->getField()->getBlocks()[FIELD_HEIGHT-1][FIELD_WIDTH-1] != NULL, @"after fill field");
     STAssertTrue(g->getField()->getBlocks()[FIELD_HEIGHT-1][FIELD_WIDTH-1]->getNumber() == g->getLastNumber(), @"after fill field");
