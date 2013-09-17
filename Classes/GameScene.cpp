e3740f87c9786f75ffa73332214e928e074099b4
nirasan <chibag+nirasan@gmail.com>
2013-09-18 07:45:14 +0900

    タッチでブロックが消えるように

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -20,6 +20,10 @@ bool GameScene::init()
     
     initGame();
     
+    // touch enable
+    setTouchEnabled(true);
+    setTouchMode(kCCTouchesOneByOne);
+    
     return true;
 }
 
@@ -32,6 +36,90 @@ void GameScene::initGame()
     game->fillBlocks();
     bool** filled = game->getFilledBlocks();
     
+    for (int x = 0; x < FIELD_WIDTH; x++) {
+        for (int y = 0; y < FIELD_HEIGHT; y++) {
+            if (filled[x][y]) {
+                Block* b = game->getField()->getBlocks()[x][y];
+                
+                CCLabelTTF* l = CCLabelTTF::create("■", "Arial", 24.0);
+                l->setPosition(ccp(size.width * (0.32 + x * 0.04), size.height * (0.1 + y * 0.06)));
+                l->setTag(b->getNumber());
+                switch (b->getColor()) {
+                    case Block::kColor_White:  l->setColor(ccc3(255, 255, 255)); break;
+                    case Block::kColor_Red:    l->setColor(ccc3(255,   0,   0)); break;
+                    case Block::kColor_Green:  l->setColor(ccc3(  0, 255,   0)); break;
+                    case Block::kColor_Blue:   l->setColor(ccc3(  0,   0, 255)); break;
+                    case Block::kColor_Yellow: l->setColor(ccc3(255, 255,   0)); break;
+                    default: break;
+                }
+                this->addChild(l);
+            }
+        }
+    }
+}
+
+bool GameScene::ccTouchBegan(CCTouch *pTouch, CCEvent *pEvent)
+{
+    return true;
+}
+
+void GameScene::ccTouchEnded(CCTouch* pTouch, CCEvent* pEvent)
+{
+    // タップポイント取得
+    CCDirector* pDirector = CCDirector::sharedDirector();
+    CCPoint touchPoint = pDirector->convertToGL(pTouch->getLocationInView());
+    
+    for (int x = 0; x < FIELD_WIDTH; x++) {
+        for (int y = 0; y < FIELD_HEIGHT; y++) {
+            Block* b = game->getField()->getBlocks()[x][y];
+            CCLabelTTF* l = (CCLabelTTF*)this->getChildByTag(b->getNumber());
+            if (!l) continue;
+            CCRect rect = l->boundingBox();
+            if (rect.containsPoint(touchPoint)) {
+                deleteBlocks(x, y);
+                return;
+            }
+        }
+    }
+}
+
+void GameScene::deleteBlocks(int x, int y)
+{
+    // 削除ブロック判定
+    game->selectDeletable(x, y);
+    
+    // 表示側削除
+    bool** deletable = game->getDeletableBlocks();
+    for (int x = 0; x < FIELD_WIDTH; x++) {
+        for (int y = 0; y < FIELD_HEIGHT; y++) {
+            if (deletable[x][y]) {
+                Block* b = game->getField()->getBlocks()[x][y];
+                this->removeChildByTag(b->getNumber());
+            }
+        }
+    }
+    
+    // ロジック側削除
+    game->deleteDeletable();
+    
+    CCSize size = CCDirector::sharedDirector()->getWinSize();
+    
+    // ブロック落下
+    game->dropBlocks();
+    for (int x = 0; x < FIELD_WIDTH; x++) {
+        for (int y = 0; y < FIELD_HEIGHT; y++) {
+            Block* b = game->getField()->getBlocks()[x][y];
+            if (b != NULL) {
+                CCLabelTTF* l = (CCLabelTTF*)this->getChildByTag(b->getNumber());
+                CCMoveTo* action = CCMoveTo::create(0.2, ccp(size.width * (0.32 + x * 0.04), size.height * (0.1 + y * 0.06)));
+                l->runAction(action);
+            }
+        }
+    }
+    
+    // ブロック埋め合わせ
+    game->fillBlocks();
+    bool** filled = game->getFilledBlocks();
     for (int x = 0; x < FIELD_WIDTH; x++) {
         for (int y = 0; y < FIELD_HEIGHT; y++) {
             if (filled[x][y]) {
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -5,6 +5,8 @@
 #include "Game.h"
 #include "Config.h"
 
+USING_NS_CC;
+
 class GameScene : public cocos2d::CCLayer
 {
 private:
@@ -15,6 +17,11 @@ public:
     virtual bool init();
     static cocos2d::CCScene* scene();
     CREATE_FUNC(GameScene);
+    
+    bool ccTouchBegan(CCTouch* pTouch, CCEvent* pEvent);
+    void ccTouchEnded(CCTouch* pTouch, CCEvent* pEvent);
+
+    void deleteBlocks(int x, int y);
 };
 
 #endif /* defined(__SameGame__GameScene__) */
